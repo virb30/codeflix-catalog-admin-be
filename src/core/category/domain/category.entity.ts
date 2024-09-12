d5a11aace3171350cd23e8e0efccf6e45e40203c
Vinicius Boscoa <virb30@gmail.com>
2024-09-12 02:16:05 +0000

    test(categories): controller integration tests

--- src/core/category/domain/category.entity.ts
+++ src/core/category/domain/category.entity.ts
@@ -1,8 +1,8 @@
-import { Entity } from "../../shared/domain/entity";
-import { ValueObject } from "../../shared/domain/value-object";
-import { Uuid } from "../../shared/domain/value-objects/uuid.vo";
-import { CategoryFakeBuilder } from "./category-fake.builder";
-import { CategoryValidatorFactory } from "./category.validator";
+import { Entity } from '../../shared/domain/entity';
+import { ValueObject } from '../../shared/domain/value-object';
+import { Uuid } from '../../shared/domain/value-objects/uuid.vo';
+import { CategoryFakeBuilder } from './category-fake.builder';
+import { CategoryValidatorFactory } from './category.validator';
 
 export type CategoryConstructorProps = {
   category_id?: Uuid;
@@ -10,73 +10,73 @@ export type CategoryConstructorProps = {
   description?: string | null;
   is_active?: boolean;
   created_at?: Date;
-}
+};
 
 export type CategoryCreateCommand = {
   name: string;
   description?: string | null;
   is_active?: boolean;
-}
+};
 
 export class Category extends Entity {
-    category_id: Uuid;
-    name: string;
-    description: string | null;
-    is_active: boolean;
-    created_at: Date;
+  category_id: Uuid;
+  name: string;
+  description: string | null;
+  is_active: boolean;
+  created_at: Date;
 
-    constructor(props: CategoryConstructorProps) {
-        super();
-        this.category_id = props.category_id ?? new Uuid();
-        this.name = props.name;
-        this.description = props.description ?? null;
-        this.is_active = props.is_active ?? true;
-        this.created_at = props.created_at ?? new Date();
-    }
+  constructor(props: CategoryConstructorProps) {
+    super();
+    this.category_id = props.category_id ?? new Uuid();
+    this.name = props.name;
+    this.description = props.description ?? null;
+    this.is_active = props.is_active ?? true;
+    this.created_at = props.created_at ?? new Date();
+  }
 
-    static create(props: CategoryCreateCommand): Category {
-      const category = new Category(props);
-      category.validate(["name"]);
-      return category;
-    }
+  static create(props: CategoryCreateCommand): Category {
+    const category = new Category(props);
+    category.validate(['name']);
+    return category;
+  }
 
-    changeName(name: string): void {
-      this.name = name;
-      this.validate(["name"]);
-    }
+  changeName(name: string): void {
+    this.name = name;
+    this.validate(['name']);
+  }
 
-    changeDescription(description: string): void {
-      this.description = description;
-    }
+  changeDescription(description: string): void {
+    this.description = description;
+  }
 
-    activate() {
-      this.is_active = true;
-    }
+  activate() {
+    this.is_active = true;
+  }
 
-    deactivate() {
-      this.is_active = false;
-    }
+  deactivate() {
+    this.is_active = false;
+  }
 
-    validate(fields?: string[]) {
-        const validator = CategoryValidatorFactory.create();
-        return validator.validate(this.notification, this, fields);
-    }
+  validate(fields?: string[]) {
+    const validator = CategoryValidatorFactory.create();
+    return validator.validate(this.notification, this, fields);
+  }
 
-    static fake() {
-        return CategoryFakeBuilder;
-    }
+  static fake() {
+    return CategoryFakeBuilder;
+  }
 
-    get entity_id(): ValueObject {
-        return this.category_id;
-    }
+  get entity_id(): ValueObject {
+    return this.category_id;
+  }
 
-    toJSON() {
-      return {
-        category_id: this.category_id.id,
-        name: this.name,
-        description: this.description,
-        is_active: this.is_active,
-        created_at: this.created_at,
-      }
-    }
+  toJSON() {
+    return {
+      category_id: this.category_id.id,
+      name: this.name,
+      description: this.description,
+      is_active: this.is_active,
+      created_at: this.created_at,
+    };
+  }
 }
--- src/nest-modules/categories-module/categories.presenter.ts
+++ src/nest-modules/categories-module/categories.presenter.ts
@@ -7,14 +7,16 @@ export class CategoryPresenter {
   id: string;
   name: string;
   description: string | null;
+  is_active: boolean;
   @Transform(({ value }: { value: Date }) => value.toISOString())
   created_at: Date;
 
   constructor(output: CreateCategoryOutput) {
     this.id = output.id;
     this.name = output.name;
-    this.description = output.name;
+    this.description = output.description;
     this.created_at = output.created_at;
+    this.is_active = output.is_active;
   }
 }
 

e2f17386a5ade12c6f81bbdfeb8ffae21845c91f
Vinicius Boscoa <virb30@gmail.com>
2024-10-08 23:24:41 +0000

    feat(genre): genre sequelize repository

---
+++ src/core/genre/infra/db/sequelize/__tests__/genre-sequelize.repository.int-spec.ts
@@ -0,0 +1,385 @@
+import { GenreCategoryModel, GenreModel } from '../genre-model';
+import { GenreSequelizeRepository } from '../genre-sequelize.repository';
+import { Genre } from '../../../../domain/genre.aggregate';
+import { GenreId } from '../../../../domain/genre.aggregate';
+import { NotFoundError } from '../../../../../shared/domain/errors/not-found.error';
+import { GenreModelMapper } from '../genre-model-mapper';
+import {
+  GenreSearchParams,
+  GenreSearchResult,
+} from '../../../../domain/genre.repository';
+import { setupSequelize } from '../../../../../shared/infra/testing/helpers';
+import { CategoryModel } from '../../../../../category/infra/db/sequelize/category.model';
+import { CategorySequelizeRepository } from '../../../../../category/infra/db/sequelize/category-sequelize.repository';
+import { Category } from '../../../../../category/domain/category.aggregate';
+
+describe('GenreSequelizeRepository Integration Test', () => {
+  setupSequelize({ models: [GenreModel, GenreCategoryModel, CategoryModel] });
+  let genreRepo: GenreSequelizeRepository;
+  let categoryRepo: CategorySequelizeRepository;
+
+  beforeEach(async () => {
+    genreRepo = new GenreSequelizeRepository(GenreModel);
+    categoryRepo = new CategorySequelizeRepository(CategoryModel);
+  });
+
+  it('should insert a new genre', async () => {
+    const category = Category.fake().aCategory().build();
+    categoryRepo.insert(category);
+    const genre = Genre.fake()
+      .aGenre()
+      .addCategoryId(category.category_id)
+      .build();
+    await genreRepo.insert(genre);
+
+    const newGenre = await genreRepo.findById(genre.genre_id);
+    expect(newGenre!.toJSON()).toStrictEqual(genre.toJSON());
+  });
+
+  it('should bulk insert new entities', async () => {
+    const categories = Category.fake().theCategories(3).build();
+    categoryRepo.bulkInsert(categories);
+    const genres = Genre.fake()
+      .theGenres(2)
+      .addCategoryId(categories[0].category_id)
+      .addCategoryId(categories[1].category_id)
+      .addCategoryId(categories[2].category_id)
+      .build();
+    await genreRepo.bulkInsert(genres);
+
+    const newGenres = await genreRepo.findAll();
+    expect(newGenres.length).toBe(2);
+    expect(newGenres[0].toJSON()).toStrictEqual({
+      ...genres[0].toJSON(),
+      categories_id: expect.arrayContaining([
+        categories[0].category_id.id,
+        categories[1].category_id.id,
+        categories[2].category_id.id,
+      ]),
+    });
+    expect(newGenres[1].toJSON()).toStrictEqual({
+      ...genres[1].toJSON(),
+      categories_id: expect.arrayContaining([
+        categories[0].category_id.id,
+        categories[1].category_id.id,
+        categories[2].category_id.id,
+      ]),
+    });
+  });
+
+  it('should find an entity by id', async () => {
+    const category = Category.fake().aCategory().build();
+    categoryRepo.insert(category);
+    const genre = Genre.fake()
+      .aGenre()
+      .addCategoryId(category.category_id)
+      .build();
+    await genreRepo.insert(genre);
+
+    const genreFound = await genreRepo.findById(genre.genre_id);
+    expect(genre.toJSON()).toStrictEqual(genreFound!.toJSON());
+
+    await expect(genreRepo.findById(new GenreId())).resolves.toBeNull();
+  });
+
+  it('should return all genres', async () => {
+    const category = Category.fake().aCategory().build();
+    await categoryRepo.insert(category);
+    const genre = Genre.fake()
+      .aGenre()
+      .addCategoryId(category.category_id)
+      .build();
+    await genreRepo.insert(genre);
+    const genres = await genreRepo.findAll();
+    expect(genres).toHaveLength(1);
+  });
+
+  it('should throw error on update when genre not found', async () => {
+    const genre = Genre.fake().aGenre().build();
+    await expect(genreRepo.update(genre)).rejects.toThrow(
+      new NotFoundError(genre.genre_id.id, Genre),
+    );
+  });
+
+  it('should update a genre', async () => {
+    const categories = Category.fake().theCategories(2).build();
+    await categoryRepo.bulkInsert(categories);
+    const genre = Genre.fake()
+      .aGenre()
+      .addCategoryId(categories[0].category_id)
+      .build();
+    await genreRepo.insert(genre);
+
+    genre.changeName('Genre updated');
+    await genreRepo.update(genre);
+
+    let genreFound = await genreRepo.findById(genre.genre_id);
+    expect(genre.toJSON()).toStrictEqual(genreFound!.toJSON());
+
+    genre.syncCategoriesId([categories[1].category_id]);
+    await genreRepo.update(genre);
+
+    genreFound = await genreRepo.findById(genre.genre_id);
+    expect(genre.toJSON()).toStrictEqual(genreFound!.toJSON());
+  });
+
+  it('should throw error on delete when a genre not found', async () => {
+    const genreId = new GenreId();
+    await expect(genreRepo.delete(genreId)).rejects.toThrow(
+      new NotFoundError(genreId.id, Genre),
+    );
+  });
+
+  it('should delete a genre', async () => {
+    const category = Category.fake().aCategory().build();
+    await categoryRepo.insert(category);
+    const genre = Genre.fake()
+      .aGenre()
+      .addCategoryId(category.category_id)
+      .build();
+    await genreRepo.insert(genre);
+
+    await genreRepo.delete(genre.genre_id);
+    await expect(genreRepo.findById(genre.genre_id)).resolves.toBeNull();
+  });
+
+  // describe('search method tests', () => {
+  //   it('should only apply paginate when other params are null', async () => {
+  //     const created_at = new Date();
+  //     const genres = Genre.fake()
+  //       .theGenres(16)
+  //       .withName('Movie')
+  //       .withCreatedAt(created_at)
+  //       .build();
+  //     await repository.bulkInsert(genres);
+  //     const spyToEntity = jest.spyOn(GenreModelMapper, 'toEntity');
+  //     const searchOutput = await repository.search(new GenreSearchParams());
+  //     expect(searchOutput).toBeInstanceOf(GenreSearchResult);
+  //     expect(spyToEntity).toHaveBeenCalledTimes(15);
+  //     expect(searchOutput.toJSON()).toMatchObject({
+  //       total: 16,
+  //       current_page: 1,
+  //       last_page: 2,
+  //       per_page: 15,
+  //     });
+  //     searchOutput.items.forEach((item) => {
+  //       expect(item).toBeInstanceOf(Genre);
+  //       expect(item.genre_id).toBeDefined();
+  //     });
+  //     const items = searchOutput.items.map((item) => item.toJSON());
+  //     expect(items).toMatchObject(
+  //       new Array(15).fill({
+  //         name: 'Movie',
+  //         description: null,
+  //         is_active: true,
+  //         created_at,
+  //       }),
+  //     );
+  //   });
+
+  //   it('should order by created_at DESC when search params are null', async () => {
+  //     const created_at = new Date();
+  //     const genres = Genre.fake()
+  //       .theGenres(16)
+  //       .withName((index) => `Movie ${index}`)
+  //       .withDescription(null)
+  //       .withCreatedAt((index) => new Date(created_at.getTime() + index))
+  //       .build();
+  //     await repository.bulkInsert(genres);
+  //     const searchOutput = await repository.search(new GenreSearchParams());
+  //     const items = searchOutput.items.map((item) => item.toJSON());
+  //     [...items].reverse().forEach((item, index) => {
+  //       expect(`${item.name}`).toBe(`${genres[index + 1].name}`);
+  //     });
+  //   });
+
+  //   it('should apply paginate and filter', async () => {
+  //     const genres = [
+  //       Genre.fake()
+  //         .aGenre()
+  //         .withName('test')
+  //         .withCreatedAt(new Date(new Date().getTime() + 5000))
+  //         .build(),
+  //       Genre.fake()
+  //         .aGenre()
+  //         .withName('a')
+  //         .withCreatedAt(new Date(new Date().getTime() + 4000))
+  //         .build(),
+  //       Genre.fake()
+  //         .aGenre()
+  //         .withName('TEST')
+  //         .withCreatedAt(new Date(new Date().getTime() + 3000))
+  //         .build(),
+  //       Genre.fake()
+  //         .aGenre()
+  //         .withName('TeSt')
+  //         .withCreatedAt(new Date(new Date().getTime() + 1000))
+  //         .build(),
+  //     ];
+
+  //     await repository.bulkInsert(genres);
+
+  //     let searchOutput = await repository.search(
+  //       new GenreSearchParams({
+  //         page: 1,
+  //         per_page: 2,
+  //         filter: 'TEST',
+  //       }),
+  //     );
+
+  //     expect(searchOutput.toJSON(true)).toMatchObject(
+  //       new GenreSearchResult({
+  //         items: [genres[0], genres[2]],
+  //         total: 3,
+  //         current_page: 1,
+  //         per_page: 2,
+  //       }).toJSON(true),
+  //     );
+
+  //     searchOutput = await repository.search(
+  //       new GenreSearchParams({
+  //         page: 2,
+  //         per_page: 2,
+  //         filter: 'TEST',
+  //       }),
+  //     );
+
+  //     expect(searchOutput.toJSON(true)).toMatchObject(
+  //       new GenreSearchResult({
+  //         items: [genres[3]],
+  //         total: 3,
+  //         current_page: 2,
+  //         per_page: 2,
+  //       }).toJSON(true),
+  //     );
+  //   });
+
+  //   it('should paginate and sort', async () => {
+  //     expect(repository.sortableFields).toStrictEqual(['name', 'created_at']);
+
+  //     const genres = [
+  //       Genre.fake().aGenre().withName('b').build(),
+  //       Genre.fake().aGenre().withName('a').build(),
+  //       Genre.fake().aGenre().withName('d').build(),
+  //       Genre.fake().aGenre().withName('e').build(),
+  //       Genre.fake().aGenre().withName('c').build(),
+  //     ];
+  //     await repository.bulkInsert(genres);
+
+  //     const arrange = [
+  //       {
+  //         params: new GenreSearchParams({
+  //           page: 1,
+  //           per_page: 2,
+  //           sort: 'name',
+  //         }),
+  //         result: new GenreSearchResult({
+  //           items: [genres[1], genres[0]],
+  //           total: 5,
+  //           current_page: 1,
+  //           per_page: 2,
+  //         }),
+  //       },
+  //       {
+  //         params: new GenreSearchParams({
+  //           page: 2,
+  //           per_page: 2,
+  //           sort: 'name',
+  //         }),
+  //         result: new GenreSearchResult({
+  //           items: [genres[4], genres[2]],
+  //           total: 5,
+  //           current_page: 2,
+  //           per_page: 2,
+  //         }),
+  //       },
+  //       {
+  //         params: new GenreSearchParams({
+  //           page: 1,
+  //           per_page: 2,
+  //           sort: 'name',
+  //           sort_dir: 'desc',
+  //         }),
+  //         result: new GenreSearchResult({
+  //           items: [genres[3], genres[2]],
+  //           total: 5,
+  //           current_page: 1,
+  //           per_page: 2,
+  //         }),
+  //       },
+  //       {
+  //         params: new GenreSearchParams({
+  //           page: 2,
+  //           per_page: 2,
+  //           sort: 'name',
+  //           sort_dir: 'desc',
+  //         }),
+  //         result: new GenreSearchResult({
+  //           items: [genres[4], genres[0]],
+  //           total: 5,
+  //           current_page: 2,
+  //           per_page: 2,
+  //         }),
+  //       },
+  //     ];
+
+  //     for (const i of arrange) {
+  //       const result = await repository.search(i.params);
+  //       expect(result.toJSON(true)).toMatchObject(i.result.toJSON(true));
+  //     }
+  //   });
+
+  //   describe('should search using filter, sort and paginate', () => {
+  //     const genres = [
+  //       Genre.fake().aGenre().withName('test').build(),
+  //       Genre.fake().aGenre().withName('a').build(),
+  //       Genre.fake().aGenre().withName('TEST').build(),
+  //       Genre.fake().aGenre().withName('e').build(),
+  //       Genre.fake().aGenre().withName('TeSt').build(),
+  //     ];
+
+  //     const arrange = [
+  //       {
+  //         search_params: new GenreSearchParams({
+  //           page: 1,
+  //           per_page: 2,
+  //           sort: 'name',
+  //           filter: 'TEST',
+  //         }),
+  //         search_result: new GenreSearchResult({
+  //           items: [genres[2], genres[4]],
+  //           total: 3,
+  //           current_page: 1,
+  //           per_page: 2,
+  //         }),
+  //       },
+  //       {
+  //         search_params: new GenreSearchParams({
+  //           page: 2,
+  //           per_page: 2,
+  //           sort: 'name',
+  //           filter: 'TEST',
+  //         }),
+  //         search_result: new GenreSearchResult({
+  //           items: [genres[0]],
+  //           total: 3,
+  //           current_page: 2,
+  //           per_page: 2,
+  //         }),
+  //       },
+  //     ];
+
+  //     beforeEach(async () => {
+  //       await repository.bulkInsert(genres);
+  //     });
+
+  //     test.each(arrange)(
+  //       'when value is $search_params',
+  //       async ({ search_params, search_result }) => {
+  //         const result = await repository.search(search_params);
+  //         expect(result.toJSON(true)).toMatchObject(search_result.toJSON(true));
+  //       },
+  //     );
+  //   });
+  // });
+});
---
+++ src/core/genre/infra/db/sequelize/genre-sequelize.repository.ts
@@ -0,0 +1,98 @@
+import { Genre, GenreId } from '../../../domain/genre.aggregate';
+import {
+  GenreSearchParams,
+  GenreSearchResult,
+  IGenreRepository,
+} from '../../../domain/genre.repository';
+import { SortDirection } from '../../../../shared/domain/repository/search-params';
+import { GenreModel } from './genre-model';
+import { GenreModelMapper } from './genre-model-mapper';
+import { NotFoundError } from '../../../../shared/domain/errors/not-found.error';
+
+export class GenreSequelizeRepository implements IGenreRepository {
+  sortableFields: string[] = ['name', 'created_at'];
+  orderBy = {
+    mysql: {
+      name: (sort_dir: SortDirection) =>
+        `binary ${this.genreModel.name}.name ${sort_dir}`,
+    },
+  };
+
+  constructor(private genreModel: typeof GenreModel) {}
+
+  async insert(entity: Genre): Promise<void> {
+    await this.genreModel.create(GenreModelMapper.toModelProps(entity), {
+      include: ['categories_id'],
+    });
+  }
+
+  async bulkInsert(entities: Genre[]): Promise<void> {
+    const models = entities.map((e) => GenreModelMapper.toModelProps(e));
+    await this.genreModel.bulkCreate(models, {
+      include: ['categories_id'],
+    });
+  }
+
+  async findById(id: GenreId): Promise<Genre | null> {
+    const model = await this._get(id.id);
+    return model ? GenreModelMapper.toEntity(model) : null;
+  }
+
+  async findAll(): Promise<Genre[]> {
+    const models = await this.genreModel.findAll({
+      include: ['categories_id'],
+    });
+    return models.map((m) => GenreModelMapper.toEntity(m));
+  }
+
+  async update(aggregate: Genre): Promise<void> {
+    const model = await this._get(aggregate.genre_id.id);
+
+    if (!model) {
+      throw new NotFoundError(aggregate.genre_id.id, this.getEntity());
+    }
+
+    await model.$remove(
+      'categories',
+      model.categories_id.map((c) => c.category_id),
+    );
+    const { categories_id, ...props } =
+      GenreModelMapper.toModelProps(aggregate);
+    await this.genreModel.update(props, {
+      where: { genre_id: aggregate.genre_id.id },
+    });
+    await model.$add(
+      'categories',
+      categories_id.map((c) => c.category_id),
+    );
+  }
+
+  async delete(id: GenreId): Promise<void> {
+    const genreCategoryRelation =
+      this.genreModel.associations.categories_id.target;
+    await genreCategoryRelation.destroy({
+      where: { genre_id: id.id },
+    });
+    const affectedRows = await this.genreModel.destroy({
+      where: { genre_id: id.id },
+    });
+
+    if (affectedRows !== 1) {
+      throw new NotFoundError(id.id, this.getEntity());
+    }
+  }
+
+  getEntity(): new (...args: any[]) => Genre {
+    return Genre;
+  }
+
+  search(props: GenreSearchParams): Promise<GenreSearchResult> {
+    throw new Error('Method not implemented.');
+  }
+
+  private async _get(id: string): Promise<GenreModel | null> {
+    return this.genreModel.findByPk(id, {
+      include: ['categories_id'],
+    });
+  }
+}
